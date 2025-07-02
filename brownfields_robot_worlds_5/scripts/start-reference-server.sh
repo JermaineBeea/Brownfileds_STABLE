e5a80f96190126ed89b97c1d566baddc823d5d52
JermaineBeea <jebeeajhb024@student.wethinkcode.co.za>
2025-07-02 12:48:51 +0200

    commit

---
+++ brownfields_robot_worlds_5/scripts/start-reference-server.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Start the reference server
+
+java -cp "/home/wtc/Documents/RepositoryAccounts/Brownfields Repositories/Brownfileds_STABLE/brownfields_robot_worlds_5/.libs/reference-server-0.1.0" za.co.wethinkcode.robots.server.Server
